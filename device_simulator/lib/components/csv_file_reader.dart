45dc5aaa8c741e44fd68e66ca697eb6545dca205
Radu-Augustin Vele <vele.ne.radu@student.utcluj.ro>
2023-11-27 12:10:48 +0200

    fix input file path retrieval (app Documents)

--- device_simulator/lib/components/csv_file_reader.dart
+++ device_simulator/lib/components/csv_file_reader.dart
@@ -3,13 +3,17 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:device_simulator/components/amqp_writer.dart';
-import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 class CsvFileReader {
   bool _keepReading = false;
-  final File _inputFile = File("./input/sensor.csv");
   int _readLines = 0;
   AmpqWriter ampqWriter = AmpqWriter();
+
+  Future<File> get _inputFile async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/flutter/sensor.csv");
+  }
 
   setKeepReading(bool value) {
     _keepReading = value;
@@ -23,12 +27,12 @@ class CsvFileReader {
   readOneEntryPeriodically(int periodSec, String deviceId, int offset,
       Function(String) updateLog) async {
     _readLines = offset;
-    Timer.periodic(Duration(seconds: periodSec), (timer) {
+    Timer.periodic(Duration(seconds: periodSec), (timer) async {
       if (!_keepReading) {
         timer.cancel();
         return;
       }
-      Stream<List<int>> inputStream = _inputFile.openRead();
+      Stream<List<int>> inputStream = (await _inputFile).openRead();
       inputStream
           .transform(utf8.decoder)
           .transform(const LineSplitter())
